aff2b2da61d1823364c29b28952ea452a494fbf6
Raghu <raghu15sep99@gmail.com>
2019-09-08 21:23:36 +0530

    Sample Code

---
+++ index.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.corpus import PlaintextCorpusReader
+# Need to download these first
+# nltk.download('punkt')
+# nltk.download('stopwords')
+sentence = '''Hi, folks how is it going. \
+Folks wassup'''
+tokens = nltk.word_tokenize(sentence)
+text = nltk.Text(tokens)
+text.concordance('folks')
+print(tokens)
+print(list(nltk.bigrams(text))) # get bigrams
+f = open('corpus/g0pA_taska.txt', 'r')
+raw = f.read()
+tokens = nltk.word_tokenize(raw)
+text = nltk.Text(tokens)
+text.concordance('the')
+# vocabulary size, "set()" removes repetitions
+print(len(set(text)))
+fdist = nltk.FreqDist(text)
+print(fdist.most_common(10)) # frequency of tokens
+corpus_root = 'corpus/'
+# loading the corpus strict regx would be: '([a-z]|[0-9]|[A-Z]|_)*.txt'
+filelists = PlaintextCorpusReader(corpus_root, '.*.txt')
+print(filelists.fileids())
